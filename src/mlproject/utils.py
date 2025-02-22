ce830b263b1efa5256ec9b08240b4eef244a7163
Prerna2411 <143021189+Prerna2411@users.noreply.github.com>
2025-02-23 00:01:13 +0530

    Utility functions

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -3,7 +3,9 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
-
+from src.mlproject.exception import CustomException
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 
 def save_object(file_path,obj):
     try:
@@ -14,3 +16,43 @@ def save_object(file_path,obj):
             dill.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            print(f"Evaluating: {model_name} - Type: {type(model)}")
+
+            if model_name in param and param[model_name]:  # Check if model needs hyperparameter tuning
+                gs = GridSearchCV(model, param[model_name], cv=3)
+                gs.fit(X_train, y_train)
+                model.set_params(**gs.best_params_)
+            
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+
+
