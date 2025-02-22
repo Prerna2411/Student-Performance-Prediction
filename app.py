94c5f2fcc14019eb8716db1d07693b70c1294ac9
Prerna2411 <143021189+Prerna2411@users.noreply.github.com>
2025-02-22 23:56:36 +0530

    flask api for model prediction

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
 import sys
 import os
 
-# Add the "src" folder to the Python path
-sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "src")))
+from flask import Flask,render_template,request
+from src.mlproject.pipelines.prediction_pipeline import PredictPipeline
 
-from mlproject.logger import logging
+import numpy as np
+import pandas as pd
 
+from sklearn.preprocessing import StandardScaler
+from src.mlproject.pipelines.prediction_pipeline import CustomData
+application=Flask(__name__)
+app=application
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-from src.mlproject.logger import logging
-from src.mlproject.exception import CustomException
-import sys
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template("predict.html")
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('reading_score')),
+            writing_score=float(request.form.get('writing_score'))
+        )
+        pred_df=data.get_data_as_data_frame()
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+    
 
-
-
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Custom Exception")
-        raise CustomException(e,sys)
-
-
-
+# Run Flask App
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+    
